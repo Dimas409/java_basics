d1fcc663a6b2abbaf6279855da00bb43442222e8
Kuznetsov Dmitry <kuzznec1989@gmail.com>
2022-02-02 21:08:51 +0800

    some code

--- Encapsulation/04_Copy/src/ru/skillbox/Main.java
+++ Encapsulation/04_Copy/src/ru/skillbox/Main.java
@@ -3,5 +3,15 @@ package ru.skillbox;
 public class Main {
 
     public static void main(String[] args) {
+        Dimensions dimensions = new Dimensions(22, 23, 25);
+        dimensions.Volume();
+        System.out.println(dimensions.Volume());
+
+        CargoInfo cargoInfo = new CargoInfo(25,45, "Lenina Street 12", "Don't flip", "Fragile cargo");
+        System.out.println(cargoInfo.getAbilityToFlip());
+        System.out.println(cargoInfo.getFragileCargo());
+        System.out.println(cargoInfo.getDeliveryAddress());
+        System.out.println(cargoInfo.getDimensions());
+        System.out.println(cargoInfo.getMass());
     }
 }
