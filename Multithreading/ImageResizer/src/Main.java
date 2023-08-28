c981c53d5ddb2a2ecf7f764fcb43cd4a8583cffe
Dimas409 <82093956+Dimas409@users.noreply.github.com>
2023-08-28 16:58:14 +0300

    Добавил статическую переменную AVAILABLE_PROCESSORS, теперь количество файлов делится ровно на количество доступных логических ядер на процессоре.

--- Multithreading/ImageResizer/src/Main.java
+++ Multithreading/ImageResizer/src/Main.java
@@ -7,22 +7,23 @@ public class Main {
     public static String DST_FOLDER = "C:\\Users\\User\\IdeaProjects\\java_basics\\Multithreading\\ImageResizer\\dstImages";
     public static String SRC_FOLDER = "C:\\Users\\User\\IdeaProjects\\java_basics\\Multithreading\\ImageResizer\\sourseImages";
     public static long START = System.currentTimeMillis();
-
+    public  static int AVAILABLE_PROCESSORS = Runtime.getRuntime().availableProcessors();
     public static void main(String[] args) {
         File srcDir = new File(SRC_FOLDER);
         File[] files = srcDir.listFiles();
 
-        for (int i = 0; i < 6; i++) {
+        for (int i = 0; i < AVAILABLE_PROCESSORS; i++) {
+            assert files != null;
             resizeMethod(files);
         }
 
     }
     public static void resizeMethod(File[] files){
-        File[] file = new File[2];
+        File[] file = new File[files.length / AVAILABLE_PROCESSORS];
         System.arraycopy(files, NUMBER_OF_FILE, file,0,  file.length);
         ImageResizer imageResizer = new ImageResizer(file, NEW_WIDTH, DST_FOLDER, START, THREAD_NUMBER);
         new Thread(imageResizer).start();
-        NUMBER_OF_FILE+=2;
+        NUMBER_OF_FILE += files.length / AVAILABLE_PROCESSORS;
         THREAD_NUMBER++;
     }
 }
