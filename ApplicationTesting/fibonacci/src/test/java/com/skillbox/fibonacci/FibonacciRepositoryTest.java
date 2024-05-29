a43ba1fc399095bdf400cc39835af26988f14151
Dimas409 <82093956+Dimas409@users.noreply.github.com>
2024-05-29 22:07:11 +0300

    Практическая работа 26.7

--- ApplicationTesting/fibonacci/src/test/java/com/skillbox/fibonacci/FibonacciRepositoryTest.java
+++ ApplicationTesting/fibonacci/src/test/java/com/skillbox/fibonacci/FibonacciRepositoryTest.java
@@ -1,10 +1,18 @@
 package com.skillbox.fibonacci;
 
 import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
@@ -18,5 +26,53 @@ public class FibonacciRepositoryTest extends PostgresTestContainerInitializer {
 
     @Autowired
     EntityManager entityManager;
+
+    @Test
+    @DisplayName("Test save fibonacci Number")
+    public void testSaveFibonacciNumber() {
+        FibonacciNumber number = new FibonacciNumber(1, 1);
+        repository.save(number);
+        entityManager.flush();
+        entityManager.detach(number);
+        List<FibonacciNumber> actual = jdbcTemplate.query(
+                "SELECT * FROM fibonacci_number WHERE index = 1",
+                (rs, rowNum) -> new FibonacciNumber(rs.getInt("id"),
+                        rs.getInt("index"), rs.getInt("value"))
+        );
+        assertEquals(1, actual.size());
+        assertEquals(number, actual.get(0));
+    }
+    @Test
+    @DisplayName("Test find by index")
+    public void testFindByIndex() {
+        FibonacciNumber number = new FibonacciNumber(2, 1);
+        repository.save(number);
+        entityManager.flush();
+        entityManager.detach(number);
+
+        Optional<FibonacciNumber> foundNumber = repository.findByIndex(2);
+        assertTrue(foundNumber.isPresent());
+        assertEquals(number, foundNumber.get());
+    }
+    @Test
+    @DisplayName("Test duplicate insertion")
+    public void testDuplicateInsertion() {
+        FibonacciNumber number1 = new FibonacciNumber(3, 2);
+        FibonacciNumber number2 = new FibonacciNumber(3, 2);
+        repository.save(number1);
+        entityManager.flush();
+        entityManager.detach(number1);
+        repository.save(number2);
+        entityManager.flush();
+        entityManager.detach(number2);
+
+        List<FibonacciNumber> actual = jdbcTemplate.query(
+                "SELECT * FROM fibonacci_number WHERE index = 3",
+                (rs, rowNum) -> new FibonacciNumber(rs.getInt("id"),
+                        rs.getInt("index"), rs.getInt("value"))
+        );
+        assertEquals(1, actual.size());
+        assertEquals(number1, actual.get(0));
+    }
 
 }
--- ApplicationTesting/fibonacci/src/test/java/com/skillbox/fibonacci/FibonacciServiceTest.java
+++ ApplicationTesting/fibonacci/src/test/java/com/skillbox/fibonacci/FibonacciServiceTest.java
@@ -1,5 +1,68 @@
 package com.skillbox.fibonacci;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
 public class FibonacciServiceTest {
 
+    @Mock
+    private FibonacciRepository repository;
+    @Mock
+    private FibonacciCalculator calculator;
+    @InjectMocks
+    private FibonacciService service;
+
+    @BeforeEach
+    public void setUp() {
+        service = new FibonacciService(repository, calculator);
+    }
+
+    @Test
+    @DisplayName("Test fibonacci number from database")
+    public void testFibonacciNumberFromDatabase() {
+        int index = 5;
+        FibonacciNumber storedNumber = new FibonacciNumber(index, 5);
+        when(repository.findByIndex(index)).thenReturn(Optional.of(storedNumber));
+        FibonacciNumber result = service.fibonacciNumber(index);
+        assertEquals(storedNumber, result);
+        verify(repository).findByIndex(index);
+        verify(calculator, never()).getFibonacciNumber(index);
+    }
+    @Test
+    @DisplayName("Test fibonacci number computed and saved")
+    public void testFibonacciNumberComputedAndSaved() {
+        int index = 6;
+        int computedValue = 8;
+        FibonacciNumber computedNumber = new FibonacciNumber(index, computedValue);
+        when(repository.findByIndex(index)).thenReturn(Optional.empty());
+        when(calculator.getFibonacciNumber(index)).thenReturn(computedValue);
+        FibonacciNumber result = service.fibonacciNumber(index);
+        assertEquals(computedNumber, result);
+        verify(repository).findByIndex(index);
+        verify(calculator).getFibonacciNumber(index);
+        verify(repository).save(computedNumber);
+    }
+
+    @Test
+    @DisplayName("Test Invalid index throws exception")
+    public void testInvalidIndexTrowsException() {
+        int invalidIndex = -1;
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> service.fibonacciNumber(invalidIndex) );
+        assertEquals("Index should be greater or equal to 1", exception.getMessage());
+        verify(repository, never()).findByIndex(invalidIndex);
+        verify(calculator, never()).getFibonacciNumber(invalidIndex);
+    }
 }
